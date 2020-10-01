fd4f190c3e55c711db73e5d0f22718ab1e290458
Yesenia Ramirez <yesenia.ramirez@itsn.edu.mx>
2020-10-01 11:32:23 -0500

    Actividad de login

--- app/src/main/java/com/example/pov/MainActivity.java
+++ app/src/main/java/com/example/pov/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.pov;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnTeacher = findViewById(R.id.btnteach);
+        btnTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, loginTeacher.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Button btnStudent = findViewById(R.id.btnStudent);
+        btnStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent intent = new Intent(MainActivity.this, loginStudent.class);
+                 startActivity(intent);
+            }
+        });
+
+
+
     }
 }
