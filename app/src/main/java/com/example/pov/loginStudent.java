a13a8ccb25eff468b27ec23a249974f883b572fb
Yesenia Ramirez <yesenia.ramirez@itsn.edu.mx>
2020-10-01 12:39:28 -0500

    Avance 1

--- app/src/main/java/com/example/pov/loginStudent.java
+++ app/src/main/java/com/example/pov/loginStudent.java
@@ -1,6 +1,9 @@
 package com.example.pov;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class loginStudent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_student);
+        Button login = findViewById(R.id.btnlogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(loginStudent.this, menuTenses.class);
+                startActivity(intent);
+            }
+        });
     }
 }
