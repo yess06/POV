99fd2cb997716b0e8f8ba83900a1d15c13203358
Armandoxx123 <josearmando.mendoza@itsn.edu.mx>
2020-10-19 12:43:07 -0500

    changes

--- app/src/main/java/com/example/pov/MainActivity.java
+++ app/src/main/java/com/example/pov/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tx = findViewById(R.id.txtexample);
         Button btnTeacher = findViewById(R.id.btnteach);
         btnTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
