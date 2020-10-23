b5b51bfb8a2defec4a49043195acbdff6b35b16e
Armandoxx123 <josearmando.mendoza@itsn.edu.mx>
2020-10-23 14:09:21 -0500

    splash activity

---
+++ app/src/main/java/com/example/pov/Splashactivity.java
@@ -0,0 +1,24 @@
+package com.example.pov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashactivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
