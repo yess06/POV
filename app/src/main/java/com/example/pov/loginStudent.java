10e12fc6cbb2b143e6bedb6a05ce43230596af1e
Yesenia Ramirez <yesenia.ramirez@itsn.edu.mx>
2021-02-22 14:14:12 -0600

    Cambios

--- app/src/main/java/com/example/pov/loginStudent.java
+++ app/src/main/java/com/example/pov/loginStudent.java
@@ -125,7 +125,7 @@ public class loginStudent extends AppCompatActivity {
     public void userlog(){
         RequestQueue requestQueue1 = Volley.newRequestQueue(getApplicationContext());
         JSONObject jsonObject = new JSONObject();
-        String url2 = "http://192.168.1.70:8000/api/auth/users";
+        String url2 = "http://apipov.efa.edu.mx/service/api/auth/users";
         JsonObjectRequest objectRequest = new JsonObjectRequest(com.android.volley.Request.Method.GET, url2, null,
                 new Response.Listener<JSONObject>() {
                     @Override
